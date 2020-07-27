9f9c5d6aecf91499ef1901337034d6cbfd9a57cf
rmenares <rodrigo.menares@gmail.com>
2020-07-27 19:41:14 -0400

    cambios Liquidacion de sueldos . JS

--- CrudMvcSp/Scripts/Aplicacion/LiqSuel/LiqSuel.js
+++ CrudMvcSp/Scripts/Aplicacion/LiqSuel/LiqSuel.js
@@ -94,6 +94,8 @@ $(document).ready(function () {
                 return false;
             }
             else {
+
+                $("#SueldBas").val(new Intl.NumberFormat("de-DE").format(SuelBase));
                 //calcula el valor del Seguro de Cesantia
                 var tipRem = $("#ListTipRem").val();
                 var data = { Id_Tip_Contrato: tipRem };
@@ -106,7 +108,7 @@ $(document).ready(function () {
                         MontTrab = DatosDev["Monto_Trabajador"];
                         var sumcesa = (MontEmpl + MontTrab);
                         var ValorCesantia = ( (SuelBase * sumcesa) / 100);
-                        $("#ValCesantia").val(ValorCesantia);
+                        $("#ValCesantia").val(new Intl.NumberFormat("de-DE").format(ValorCesantia));
                     });
                 $("#DiasTrab").focus();
             }
@@ -130,40 +132,11 @@ $(document).ready(function () {
                 $("#DiasTrab").focus();
                 return false;
             }
-            else { $("#PorcCom").focus();}                                
+            else { $("#CantHrsExt").focus();}                                
         }
     })
-
     //Calculos de la Liquidacion de Sueldo
 
-    //Calulo de Comisiones
-    $("#PorcCom").on('keyup', function (e) {
-        var keycode = e.keyCode || e.which;
-        if (keycode == 13) {
-            PorcCom = $("#PorcCom").val();
-            if (isNaN(PorcCom) || (PorcCom=="")) {
-                toastr["error"](" Dato Debe Ser Numérico ", "Atención")
-                toastr.options = {
-                    "closeButton": false, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-center", "preventDuplicates": false,
-                    "onclick": null, "showDuration": "400", "hideDuration": "1000", "timeOut": "5000",
-                    "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn",
-                    "hideMethod": "fadeOut"
-                };
-                $("#PorcCom").val("");
-                $("#PorcCom").focus();
-                return false;
-            }
-            else {
-                SuelBase = $("#SueldBas").val();
-                PorcCom = ((SuelBase * PorcCom) / 100);
-                // lo convierte a entero
-                PorcCom = PorcCom | 0;
-                //muestra el Valor de la comision.
-                $("#ValorCom").val(PorcCom);
-                $("#CantHrsExt").focus();
-            }
-        }
-    })
 
     //Calcula Las Horas Extras
     $("#CantHrsExt").on('keyup', function (e) {
@@ -196,28 +169,9 @@ $(document).ready(function () {
                     TotHrsExt = (ValorHrExt * CantHrsExt);
                     //convierte a entero
                     TotHrsExt = TotHrsExt | 0;
-                    $("#ValHrsExt").val(TotHrsExt);
-
-                    // Calculo de la Gratificación
-                    var Ganancias = (SuelBase + PorcCom + TotHrsExt);
-                    var SuelDevengado = ((Ganancias * 25) / 100);
-                    //SuelDevengado = SuelDevengado | 0;
-
-                    var SueldMin = 320500;
-
-                    var GratMensual = ((SueldMin * 4, 75) / 12);
-
-                    var Grat2 = (GratMensual * 4, 75);
-
-                    var TopeGratMensual = (Grat2 / 12);
-
-                    if (SuelDevengado > TopeGratMensual) {
-                        $("#ValGrat").val(TopeGratMensual);
-                    }
-                    if (SuelDevengado < TopeGratMensual) {
-                        $("#ValGrat").val(SuelDevengado);
-                    }
-                    $("#Bonos").focus();
+                    // muestra el valor a pagar por horas extras
+                    $("#ValHrsExt").val(new Intl.NumberFormat("de-DE").format(TotHrsExt));
+                    $("#PorcCom").focus();
                 }
                 else {
                     toastr["error"](" No Puede Haber Más de 40 Horas Extras Al Mes ", "Atención")
@@ -229,11 +183,39 @@ $(document).ready(function () {
                     };
                     $("#CantHrsExt").val("")
                     $("#CantHrsExt").focus();
-                } 
+                }
             }
         }
     })
 
+    //Calulo de Comisiones
+    $("#PorcCom").on('keyup', function (e) {
+        var keycode = e.keyCode || e.which;
+        if (keycode == 13) {
+            PorcCom = $("#PorcCom").val();
+            if (isNaN(PorcCom) || (PorcCom=="")) {
+                toastr["error"](" Dato Debe Ser Numérico ", "Atención")
+                toastr.options = {
+                    "closeButton": false, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-center", "preventDuplicates": false,
+                    "onclick": null, "showDuration": "400", "hideDuration": "1000", "timeOut": "5000",
+                    "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+                $("#PorcCom").val("");
+                $("#PorcCom").focus();
+                return false;
+            }
+            else {
+                SuelBase = $("#SueldBas").val();
+                PorcCom = ((SuelBase * PorcCom) / 100);
+                // lo convierte a entero
+                PorcCom = PorcCom | 0;
+                //muestra el Valor de la comision.
+                $("#ValorCom").val(new Intl.NumberFormat("de-DE").format(PorcCom));
+                $("#Bonos").focus();
+            }
+        }
+    })
     
     $("#Bonos").on('keyup', function (e) {
         var keycode = e.keyCode || e.which;
@@ -251,27 +233,36 @@ $(document).ready(function () {
                 $("#Bonos").focus();
                 return false;
             }
-            else {$("#ValCola").focus();  }     
-        }
-    })
+            else {
+                // Calculo de la Gratificación
+                var Ganancias = (parseInt(SuelBase) + parseInt(PorcCom) + parseInt(TotHrsExt) + parseInt(Bonos));
 
-    $("#ValCola").on('keyup', function (e) {
-        var keycode = e.keyCode || e.which;
-        if (keycode == 13) {
-            ValColac = $("#ValCola").val();
-            if (isNaN(ValColac) || (ValCola =="")) {
-                toastr["error"](" Dato Debe Ser Numérico ", "Atención")
-                toastr.options = {
-                    "closeButton": false, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-center", "preventDuplicates": false,
-                    "onclick": null, "showDuration": "400", "hideDuration": "1000", "timeOut": "5000",
-                    "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn",
-                    "hideMethod": "fadeOut"
-                };
-                $("#ValCola").val("");
-                $("#ValCola").focus();
-                return false;
-            }
-            else { $("#ValMov").focus(); }
+                var SuelDevengado = ((Ganancias * 25) / 100);
+                //SuelDevengado = SuelDevengado | 0;
+
+                var SueldMin = 320500;
+
+                var GratMensual = ((SueldMin * 4.75) / 12);
+
+                var Grat2 = (GratMensual * 4.75);
+
+                var TopeGratMensual = (Grat2 / 12);
+
+                if (SuelDevengado > TopeGratMensual) {
+                    $("#ValGrat").val(new Intl.NumberFormat("de-DE").format(TopeGratMensual));
+
+                }
+                if (SuelDevengado < TopeGratMensual) {
+                    $("#ValGrat").val(new Intl.NumberFormat("de-DE").format(SuelDevengado));
+                }
+                valgrat = $("#ValGrat").val();
+
+                var TotHaberes = parseInt(Ganancias) + parseInt(valgrat);
+
+                $("#TotImponible").val(new Intl.NumberFormat("de-DE").format(TotHaberes));
+
+                $("#ValMov").focus();
+            }     
         }
     })
 
@@ -291,11 +282,62 @@ $(document).ready(function () {
                 $("#ValMov").focus();
                 return false;
             }
-            else { $("#AfpSelec").focus(); }
+            else {
+                $("#ValMov").val(new Intl.NumberFormat("de-DE").format(ValMovi));
+                $("#ValCola").focus();
+            }
+        }
+    })
+    
+    $("#ValCola").on('keyup', function (e) {
+        var keycode = e.keyCode || e.which;
+        if (keycode == 13) {
+            ValColac = $("#ValCola").val();
+            if (isNaN(ValColac) || (ValCola == "")) {
+                toastr["error"](" Dato Debe Ser Numérico ", "Atención")
+                toastr.options = {
+                    "closeButton": false, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-center", "preventDuplicates": false,
+                    "onclick": null, "showDuration": "400", "hideDuration": "1000", "timeOut": "5000",
+                    "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+                $("#ValCola").val("");
+                $("#ValCola").focus();
+                return false;
+            }
+            else {
+                $("#ValCola").val(new Intl.NumberFormat("de-DE").format(ValColac));
+                $("#ValViatico").focus();
+            }
         }
     })
 
-
+    $("#ValViatico").on('keyup', function (e) {
+        var keycode = e.keyCode || e.which;
+        if (keycode == 13) {
+            ValViat = $("#ValViatico").val();
+            if (isNaN(ValViat) || ValViat == "") {
+                toastr["error"](" Dato Debe Ser Numérico ", "Atención")
+                toastr.options = {
+                    "closeButton": false, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-center", "preventDuplicates": false,
+                    "onclick": null, "showDuration": "400", "hideDuration": "1000", "timeOut": "5000",
+                    "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+                $("#ValViatico").val("");
+                $("#ValViatico").focus();
+                return false;
+            }
+            else {
+                $("#ValViatico").val(new Intl.NumberFormat("de-DE").format(ValViat));
+                $("#AfpSelec").focus();
+            }
+            var TotHaberes = ( parseInt(ValMovi)  + parseInt(ValColac)  + parseInt(ValViat)  );
+            $("#TotHaber").val(new Intl.NumberFormat("de-DE").format(TotHaberes));
+            $("#AfpSelec").focus();
+        }
+    })
+    
     $("#AfpSelec").dblclick(function (e) {
         e.preventDefault();
         cod_afp = $("#AfpSelec").val();
@@ -310,15 +352,15 @@ $(document).ready(function () {
                 //SuelBase = $("#SueldBas").val();
                 MontAfp = ((SuelBase * PorcAfp) / 100);
                 //MontAfp = MontAfp | 0;
-                $("#MontoAfp").val(MontAfp);
+                $("#MontoAfp").val(new Intl.NumberFormat("de-DE").format(MontAfp));
             });
         $("#SalSelec").focus();
-    })
+    });
 
     $("#SalSelec").dblclick(function (e) {
         e.preventDefault();
-        cod_salud = $("#SalSelec").val();
-        var data = { Cod_Salud: cod_salud };
+        csalud = $("#SalSelec").val();
+        var data = { Cod_Salud: csalud };       
         var url = "/LiqSueld/BuscaValSal";
         $.post(url, data)
             .done(function (data) {
@@ -328,31 +370,9 @@ $(document).ready(function () {
                 PorcSalud = DatosDevSal["Porc_Cotiz"];
                 //SuelBase = $("#SueldBas").val();
                 MonSalud = ((SuelBase * PorcSalud) / 100);
-                $("#MontoSalud").val(MonSalud);
+                $("#MontoSalud").val(new Intl.NumberFormat("de-DE").format(MonSalud));
             })
-        
     })
-
-
-    
-
-
-
-    //$("#Cerrar").click(function (event) {
-    //    event.preventDefault();
-    //    RutEmple = "";
-    //    fecha = "";
-    //    SuelBase = 0;
-    //    PorcCom = 0;
-    //    CantHrsExt = 0;
-    //    totHrsExt = 0;
-    //    CodigoAfp = 0;
-    //    NomAfp = 0;
-    //    PorcAfp = 0;
-    //    MontAfp = 0;
-    //    Ganancias = 0;
-    //    SuelDevengado = 0;
-    //})
 
     // grabar datos 
     $("#BtnGrabLiqSueld").click(function (event) {
